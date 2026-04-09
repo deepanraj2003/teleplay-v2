67aa7ad625680cc910833890e2a6398e014e3424
Deepanraj Annadurai <deepanpec21@gmail.com>
2026-04-09 19:57:38 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,15 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from database import engine
-from sqlalchemy.orm import declarative_base
+from backend.database import Base
 
-Base = declarative_base()
 
 class Movie(Base):
     __tablename__ = "movies"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    name = Column(String, index=True)
     file_id = Column(String)
-
-# Create table
-Base.metadata.create_all(bind=engine)
