ac19835adaea1e903a1b5d41eff65e09e0e2b697
Deepanraj Annadurai <deepanpec21@gmail.com>
2026-04-09 19:58:16 +0530

    Update bot_runner.py

--- backend/bot_runner.py
+++ backend/bot_runner.py
@@ -1,3 +1,14 @@
-from backend.telegram_bot import start_bot
+import asyncio
+from backend.telegram_bot import create_application
 
-start_bot()
+
+async def run_bot():
+    app = create_application()
+
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    print("Bot started...")
+
+    await asyncio.Event().wait()
